11ad4672ea7229bdc73ee37ec4c66d6ba6042c5d
huiping.guo <huiping.guo@dena.jp>
2019-12-12 19:02:06 +0900

    いらないファイル削除

--- FlappyBird/GameViewController.swift
+++ FlappyBird/GameViewController.swift
@@ -15,25 +15,25 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        guard let view = self.view as? SKView else  { return }
+        
+        view.ignoresSiblingOrder = false
+        view.showsFPS = false
+        view.showsNodeCount = false
+        
+        let scene = SKScene(size: view.bounds.size)
+        
+        // Load the SKScene from 'GameScene.sks'
+        // Set the scale mode to scale to fit the window
+        scene.scaleMode = .aspectFill
+        
+        // Present the scene
+        view.presentScene(scene)
+        
     }
 
     override var shouldAutorotate: Bool {
-        return true
+        return false
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
